86e3227eaf0134195921d4f20de958701cd8fb7e
camilo forero <rc.forero@uniandes.edu.co>
2019-10-28 20:32:02 -0500

    Initial commit

---
+++ CommonEntities/ConfigEntities.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonEntities
+{
+    public class FieldsXWords
+    {
+        public List<FieldConfig> ConfigurationFields { get; set; }
+        public List<string> Bases { get; set; }
+    }
+
+    
+}
---
+++ CommonEntities/FieldConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonEntities
+{
+    public class FieldConfig
+    {
+        public string FieldName { get; set; }
+        public List<ConfigValue> MultipliersConfig { get; set; }
+        
+    }
+
+    public class ConfigValue
+    {
+        public string KeyWord { get; set; }
+        public int Multiplier { get; set; }
+    }
+}
